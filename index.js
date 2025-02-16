6337f24677e4723694e11231eeaae9c608e7ed7e
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-02-16 23:12:30 +0000

    Lint JavaScript files

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-import { cite } from './lib/cite.js';
-import { linkEmbed } from './lib/link-embed.js';
+import { cite } from "./lib/cite.js";
+import { linkEmbed } from "./lib/link-embed.js";
 
+/**
+ * Markdown rules
+ * @param {object} md - markdown-it instance
+ * @param {object} [pluginOptions] - Plugin options
+ */
+// eslint-disable-next-line unicorn/no-anonymous-default-export
 export default function (md, pluginOptions = {}) {
   // Default plugin options
   const defaults = {
-    classNameContainer: 'embed',
+    classNameContainer: "embed",
   };
 
   // Merge options
-  const options = {...defaults, ...pluginOptions};
+  const options = { ...defaults, ...pluginOptions };
 
   // Cite
-  md.core.ruler.before('inline', 'cite', state => cite(state));
+  md.core.ruler.before("inline", "cite", (state) => cite(state));
 
   // Embed
-  md.core.ruler.before('linkify', 'link_embed', state => linkEmbed(state, options));
-};
+  md.core.ruler.before("linkify", "link_embed", (state) =>
+    linkEmbed(state, options),
+  );
+}
--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
-import test from 'node:test';
-import markdownit from 'markdown-it'
-import testGenerator from 'markdown-it-testgen';
-import rules from '../index.js';
+import test from "node:test";
+import markdownit from "markdown-it";
+import testGenerator from "markdown-it-testgen";
+import rules from "../index.js";
 
 // markdown-it-testgen expects Mocha to have set the following globals
-global.describe = test.describe
-global.it = test.it
+globalThis.describe = test.describe;
+globalThis.it = test.it;
 
-describe('@paulrobertlloyd/markdown-it-rules', () => {
+describe("@paulrobertlloyd/markdown-it-rules", () => {
   const md = markdownit({
     html: true,
-  })
+  });
 
   md.use(rules);
 
-  testGenerator('./test/fixtures/cite.txt', md);
-  testGenerator('./test/fixtures/embed.txt', md);
+  testGenerator("./test/fixtures/cite.txt", md);
+  testGenerator("./test/fixtures/embed.txt", md);
 });
