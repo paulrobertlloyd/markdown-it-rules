3f222fc091c529ff2ff0c1d38cb7cc00174df8b7
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-02-16 22:56:10 +0000

    Use Node’s test runner in place of mocha and c8

--- test/test.js
+++ test/test.js
@@ -1,7 +1,11 @@
-import {describe} from 'mocha';
+import test from 'node:test';
 import markdownit from 'markdown-it'
 import testGenerator from 'markdown-it-testgen';
 import rules from '../index.js';
+
+// markdown-it-testgen expects Mocha to have set the following globals
+global.describe = test.describe
+global.it = test.it
 
 describe('@paulrobertlloyd/markdown-it-rules', () => {
   const md = markdownit({
