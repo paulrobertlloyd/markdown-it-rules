f5b3fb9a76ce101c50c4fa1ec315b6164042835a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-09-20 15:28:20 +0100

    Fix JSDoc and type checking errors

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import { cite } from "./lib/cite.js";
 import { linkEmbed } from "./lib/link-embed.js";
 
-/** @typedef {import('markdown-it').default} MarkdownIt */
-/** @typedef {import('markdown-it').Options} MarkdownItOptions */
+/**
+ * @import MarkdownIt from "markdown-it"
+ */
+
+/**
+ * @typedef {ReturnType<typeof MarkdownIt>} MarkdownItInstance
+ */
+
+/**
+ * @typedef {object} PluginOptions
+ * @property {string} [classNameContainer="embed"] - Class name for embed container
+ */
 
 /**
  * Markdown rules
- * @param {MarkdownIt} md - markdown-it instance
- * @param {MarkdownItOptions} [pluginOptions] - Plugin options
+ * @param {MarkdownItInstance} md - markdown-it instance
+ * @param {PluginOptions} [pluginOptions] - Plugin options
  */
 // eslint-disable-next-line unicorn/no-anonymous-default-export
 export default function (md, pluginOptions = {}) {
